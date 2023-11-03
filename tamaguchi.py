4d6b25cc525aeedbb89839649400457136d8914e
Débora Campos <127258657+deboracamposs@users.noreply.github.com>
2023-11-02 22:37:16 -0300

    Add files via upload

---
+++ tamaguchi.py
@@ -0,0 +1,9 @@
+from bibliotecatmg import Pessoa
+pessoa=Pessoa('Arlene', '98', 28)
+pessoa.apresenta()
+pessoa.comer()
+pessoa.pararDeComer()
+pessoa.falar()
+pessoa.pararDeFalar()
+pessoa.dormir()
+pessoa.acordar()
